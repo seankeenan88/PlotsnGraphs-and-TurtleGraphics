29d575c2f01b82e822b5716541ff3bc8818631c5
Seán Keenan <96420993+seankeenan88@users.noreply.github.com>
2023-03-03 00:57:17 +0000

    Add files via upload

---
+++ Graphing.py
@@ -0,0 +1,9 @@
+print(score_list2)
+print(score_list1)
+plt.plot(score_list1,label = "player 1 score")
+plt.plot(score_list2,label = "player 2 score")
+plt.title('Score Graph')
+plt.xlabel('Game number')
+plt.ylabel('Score')
+plt.show()
+                                      
---
+++ Python Turtle Graphics 130921.py
@@ -0,0 +1,23 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: A program to demonstrate the use of turtle graphics
+
+# import the turtle graphics library
+
+
+from turtle import *
+color('blue')
+forward (100)
+left (60)
+forward (100)
+left (60)
+forward (100)
+color ('red')
+left (60)
+forward (100)
+left (60)
+forward (100)
+left (60)
+forward (100)
